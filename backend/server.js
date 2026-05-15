9664d8743d76f7e54c1c558798879f218e38defe
Shavidu Keshan <shavidu4321@gmail.com>
2026-05-15 08:52:55 +0530

    Add MongoDB dependency and configure database connection💥

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log("Database not connected",err);
+  });
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +23,3 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.get('/api/data', (req, res) => {
-  res.json({ message: 'Hello from the backend!' });
-});
